ecc9fe73be7a0dfbc3f299ac09208ab05b9e4f33
Vlad Deev <vlad.deev@gmail.com>
2019-05-14 12:24:55 -0400

    Implement unlayoutCallback. Add annotations.

--- extensions/amp-insticator/0.1/amp-insticator.js
+++ extensions/amp-insticator/0.1/amp-insticator.js
@@ -25,11 +25,14 @@ import { setStyles } from '../../../src/style';
 /* ++++++++++ --------------- EXPORT AMP-INSTICATOR --------------- ++++++++++ */
 export class AmpInsticator extends AMP.BaseElement {
 
-
-  // ..... CONSTRUCTOR ..... //
+  /** @param {!AmpElement} element */
   constructor(element) {
     super(element);
+
+    /** @private {?Element} */
     this.iFrameElement = null; // amp requirement
+
+    /** @private {!Object} */
     this.url = {
       ads: 'https://drhn9v8cwg89y.cloudfront.net',
       embed: 'https://d3lcz8vpax4lo2.cloudfront.net'
@@ -38,29 +41,37 @@ export class AmpInsticator extends AMP.BaseElement {
 
 
   // ~~~~~ AMP METHODs ~~~~~ //
-  preconnectCallback() {
+  /**
+   * @param {boolean=} onLayout
+   * @override
+   */
+  preconnectCallback(onLayout) {
     // define preconnect
-    const {preconnect} = this;
+    const { preconnect } = this;
     // host where we store advertising settings code
     preconnect.url(this.url.ads);
     // host we serve the core embed application from
     preconnect.url(this.url.embed);
   }
+
+  /** @override */
   buildCallback() {
     // create markup structure
     const template = this.createTemplate(this.createInitialMarkup());
     // append markup structure to DOM
     this.appendElement(this.element, template);
   }
+
+  /** @override */
   layoutCallback() {
     // get data attribute from the amp-insticator tag
     const embedId = this.element.getAttribute('embed-id');
     // store DOM elements
-    const insticatorContainer = this.element.querySelector('#insticator-container'); 
+    const insticatorContainer = this.element.querySelector('#insticator-container');
     const embedIframe = this.iFrameElement = this.element.querySelector('#insticator-iframe');
     const embedApp = this.createElement(this.element.ownerDocument, 'div', { id: 'app' });
     const embedScript = this.createElement(this.element.ownerDocument, 'script', { type: 'text/javascript', src: `${this.url.embed}/embed-code/${embedId}.js` });
-    
+
     // append iframe
     this.appendElement(this.element.querySelector('#insticator-embed'), embedIframe);
 
@@ -74,34 +85,80 @@ export class AmpInsticator extends AMP.BaseElement {
     // apply custom styles
     setStyles(insticatorContainer, { 'text-align': 'center' });
   }
+
+  /** @override */
   isLayoutSupported(layout) {
     return layout == Layout.CONTAINER;
   }
 
+  /** @override */
+  unlayoutCallback() {
+    if (this.iFrameElement) {
+      removeElement(this.iFrameElement);
+      this.iFrameElement= null;
+      this.iframePromise_ = null;
+    }
+    return true; // Call layoutCallback again.
+  }
+
 
   // ----- CUSTOM FUNCTIONs ----- //
+  /**
+   * Make get request to get ads data.
+   * @param {!string} file
+   * @param {function(Object)} callback
+   */
   getRequest(file, callback) {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => xhr.readyState == 4 && xhr.status == 200 ? callback(JSON.parse(xhr.responseText)) : null;
     xhr.open('GET', file, true);
     xhr.send();
   }
+
+  /**
+   * Create markup structure.
+   * @param {!string} domString
+   * @return {!Element}
+   */
   createTemplate(domString) {
     const template = document.createElement('template');
     template.innerHTML = domString;
     return template.content;
   }
+
+  /**
+   * Create an element with attributes.
+   * @param {!Element} location
+   * @param {!string} el
+   * @param {!Object} attrs
+   * @return {!Element}
+   */
   createElement(location, el, attrs) {
     const newEl = location.createElement(el);
     Object.entries(attrs).forEach(attr => newEl.setAttribute(attr[0], attr[1]));
     return newEl;
   }
+
+  /**
+   * Append a node to a specified parent node.
+   * @param {!Element} location
+   * @param {!Element} el
+   */
   appendElement(location, el) {
     location.appendChild(el);
   }
+
+  /**
+   * Append ads.
+   * @param {!Object} ads
+   */
   appendAds(ads) {
     Object.entries(ads).forEach(ad => this.appendElement(this.element.querySelector(`#div-insticator-ad-${ad[0][ad[0].length -1]}`), this.createElement(this.element.ownerDocument, 'amp-ad', ad[1])));
   }
+
+  /**
+   * @return {!string}
+   */
   createInitialMarkup() {
     return `
       <div id="insticator-container">
